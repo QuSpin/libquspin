94f712bc0c950f8f62c1770d277f2a8c7aed7ac2
Phillip Weinberg <weinbe58@gmail.com>
2024-09-29 17:59:39 +0000

    refactor operator constructor.

--- source/qoperator/qoperator.cpp
+++ source/qoperator/qoperator.cpp
@@ -10,26 +10,46 @@
 
 namespace quspin {
 
+  decltype(auto) select_data_variants(const Array &data) {
+    using namespace details;
+
+    return select<array<int8_t>, array<int16_t>, array<float>, array<double>,
+                  array<cfloat>, array<cdouble>>(data.get_variant_obj());
+  }
+  
+  decltype(auto) select_index_variants(const Array &indptr) {
+    using namespace details;
+
+    return select<array<int32_t>, array<int64_t>>(indptr.get_variant_obj());
+  }
+
+  decltype(auto) select_cindex_variants(const Array &cindices) {
+    using namespace details;
+
+    return select<array<uint8_t>, array<uint16_t>>(cindices.get_variant_obj());
+  }
+
   Operator::Operator(Array data, Array indptr, Array indices, Array cindices) {
     using namespace details;
 
     const std::size_t dim = indptr.size() - 1;
 
-    auto data_variants = select<array<int8_t>, array<int16_t>, array<float>, array<double>,
-                                        array<cfloat>, array<cdouble>>(data.get_variant_obj());
-    auto indptr_variants = select<array<int32_t>, array<int64_t>>(indptr.get_variant_obj());
-    auto indices_variants = select<array<int32_t>, array<int64_t>>(indices.get_variant_obj());
-    auto cindices_variants = select<array<uint8_t>, array<uint16_t>>(cindices.get_variant_obj());
+    auto data_variants = select_data_variants(data);
+    auto indptr_variants = select_index_variants(indptr);
+    auto indices_variants = select_index_variants(indices);
+    auto cindices_variants = select_cindex_variants(cindices);
+
+    auto constructor = [&dim](auto &&data, auto &&indptr, auto &&indices, auto &&cindices) {
+      if constexpr (!std::is_same_v<decltype(indptr), decltype(indices)>) {
+        return ErrorOr<qoperators>(Error(ErrorType::ValueError, "indptr and indices must have the same dtype"));
+      } else {
+        qoperators op = qoperator(dim, data, indptr, indices, cindices);
+        return ErrorOr<qoperators>(op);
+      }
+    };
 
     this->internals_ = visit_or_error<qoperators>(
-        [&dim](auto &&data, auto &&indptr, auto &&indices, auto &&cindices) {
-          if constexpr (!std::is_same_v<decltype(indptr), decltype(indices)>) {
-            return ErrorOr<qoperators>(Error(ErrorType::ValueError, "indptr and indices must have the same dtype"));
-          } else {
-            qoperators op = qoperator(dim, data, indptr, indices, cindices);
-            return ErrorOr<qoperators>(op);
-          }
-        },
+        constructor,
         data_variants, indptr_variants, indices_variants, cindices_variants);
   }
 
