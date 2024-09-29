453d3a06bb045a72cc759e20d89999be5a897719
Phillip Weinberg <weinbe58@gmail.com>
2024-09-29 10:13:03 -0400

    removing more references to "greeter"

--- documentation/conf.py
+++ documentation/conf.py
@@ -10,10 +10,10 @@ LINKS_NAVBAR1 = [
 
 # LINKS_NAVBAR1 = [
 #     (None, 'pages', [(None, 'about')]),
-#     (None, 'namespaces', [(None, 'namespacegreeter')]),
+#     (None, 'namespaces', [(None, 'namespacequspin')]),
 # ]
 #
 # LINKS_NAVBAR2 = [
-#     (None, 'annotated', [(None, 'classgreeter_1_1_greeter')]),
-#     (None, 'files', [(None, 'greeter_8h')]),
+#     (None, 'annotated', [(None, 'classquspin_1_1_quspin')]),
+#     (None, 'files', [(None, 'quspin_8h')]),
 # ]
