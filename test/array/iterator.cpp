72d8bdf5815c4b93297321243ffdba977532ef9a
Phillip Weinberg <weinbe58@gmail.com>
2024-11-01 20:37:46 -0400

    updating iterator test.

--- test/array/iterator.cpp
+++ test/array/iterator.cpp
@@ -1,23 +1,48 @@
 
+#include <chrono>
 #include <iostream>
 #include <quspin/array/details/array.hpp>
+#include <tuple>
+#include <vector>
 
 using namespace quspin::details;
 
-int main() {
-  array<float> arr(std::vector<std::size_t>({1000000}));
+template <typename Func, typename... Args> decltype(auto) time_func(Func func, Args... args) {
+  auto start = std::chrono::high_resolution_clock::now();
+  auto res = func(args...);
+  auto stop = std::chrono::high_resolution_clock::now();
+  return std::make_tuple(res, stop - start);
+}
 
-  //   int i = 0;
+std::size_t test(array<float> &arr) {
+  std::size_t i = 0;
   for (auto it = arr.begin(); it != arr.end(); ++it) {
-    auto dim_indices = it.dim_indices();
-    std::cout << *it << " " << it.index_();
-    // for (auto &dim_index : dim_indices) {
-    //   std::cout << " " << dim_index;
-    // }
-    std::cout << std::endl;
-    // i++;
-    // if(i > 10000) break;
+    i += it.index_();
   }
+  return i;
+}
+
+int main() {
+  std::vector<std::size_t> shape0 = {100, 100, 100};
+  array<float> arr0(shape0);
+
+  auto [res0, time0] = time_func(test, arr0);
+
+  std::cout << "Time: " << time0.count() << "ns" << std::endl;
+
+  std::vector<std::size_t> shape1 = {100, 10000};
+  array<float> arr1(shape1);
+
+  auto [res1, time1] = time_func(test, arr1);
+
+  std::cout << "Time: " << time1.count() << "ns" << std::endl;
+
+  std::vector<std::size_t> shape2 = {1000000};
+  array<float> arr2(shape2);
+
+  auto [res2, time2] = time_func(test, arr2);
+
+  std::cout << "Time: " << time2.count() << "ns" << std::endl;
 
   return 0;
 }
