5a3f2df8a78e1a6611a6d7d27fb2e33886b0c171
Phillip Weinberg <weinbe58@gmail.com>
2024-11-23 14:28:12 -0500

    poping stash

--- include/quspin/qmatrix/kernel/details/sum.hpp
+++ include/quspin/qmatrix/kernel/details/sum.hpp
@@ -12,19 +12,18 @@ namespace quspin {
   namespace details {
 
     template <typename Op, PrimativeTypes T, PrimativeTypes I, PrimativeTypes J>
-    struct row_sum_tasks {
-      Op &&op;
-      const qmatrix<T, I, J> lhs;
-      const qmatrix<T, I, J> rhs;
-      I *indptr;
+    void elementwise_binop_size(Op &&op, const qmatrix<T, I, J> &lhs,
+                                const qmatrix<T, I, J> &rhs,
+                                array<I> &out_indptr,
+                                const std::size_t num_threads = 0) {
+      assert(lhs.dim() == rhs.dim());
+      assert(lhs.dim() == out_indptr.size() - 1);
 
-    public:
-      row_sum_tasks(Op &&op, const qmatrix<T, I, J> &lhs, const qmatrix<T, I, J> &rhs, I *indptr)
-          : op(op), lhs(lhs), rhs(rhs), indptr(indptr) {}
+      I *indptr = out_indptr.mut_data();
+      indptr[0] = 0;
 
-      std::size_t size() const { return lhs.dim(); }
-
-      void do_work(const std::size_t row_index, const std::size_t) {
+#pragma omp parallel for num_threads(num_threads)
+      for (std::size_t row_index = 0; row_index < lhs.dim(); ++row_index) {
         I sum_size = 0;
 
         iterator<T, I, J> lhs_row_begin = lhs.row_begin(row_index);
@@ -50,24 +49,9 @@ namespace quspin {
             ++rhs_row_begin;
           }
         }
-        indptr[row_index + 1]
-            = cast<I>(sum_size + (lhs_row_end - lhs_row_begin) + (rhs_row_end - rhs_row_begin));
+        indptr[row_index + 1] = cast<I>(sum_size + (lhs_row_end - lhs_row_begin)
+                                        + (rhs_row_end - rhs_row_begin));
       }
-    };
-
-    template <typename Op, PrimativeTypes T, PrimativeTypes I, PrimativeTypes J>
-    void elementwise_binop_size(Op &&op, const qmatrix<T, I, J> &lhs, const qmatrix<T, I, J> &rhs,
-                                array<I> &out_indptr, const std::size_t num_threads = 0) {
-      assert(lhs.dim() == rhs.dim());
-      assert(lhs.dim() == out_indptr.size() - 1);
-
-      I *indptr = out_indptr.mut_data();
-      indptr[0] = 0;
-
-      row_sum_tasks<Op, T, I, J> tasks(op, lhs, rhs, indptr);
-      WorkQueue task_runner(tasks);
-
-      task_runner.run(Schedule::SequentialBlocks, num_threads);
 
       // cumulaive sum
       for (std::size_t row_index = 1; row_index <= lhs.dim(); ++row_index) {
@@ -76,20 +60,20 @@ namespace quspin {
     }
 
     template <typename Op, PrimativeTypes T, PrimativeTypes I, PrimativeTypes J>
-    class populate_row_tasks {
-      Op &&op;
-      const qmatrix<T, I, J> lhs;
-      const qmatrix<T, I, J> rhs;
-      qmatrix<T, I, J> out;
+    void elementwise_binary_operation(Op &&op, const qmatrix<T, I, J> &lhs,
+                                      const qmatrix<T, I, J> &rhs,
+                                      qmatrix<T, I, J> &out,
+                                      const std::size_t num_threads = 0) {
+      static_assert(std::is_invocable_v<Op, T, T>,
+                    "Incompatible Operator with input types");
+      static_assert(
+          std::is_same_v<T, std::decay_t<std::invoke_result_t<Op, T, T>>>,
+          "Incompatible output type");
+      assert(lhs.dim() == rhs.dim());
+      assert(lhs.dim() == out.dim());
 
-    public:
-      populate_row_tasks(Op &&op, const qmatrix<T, I, J> &lhs, const qmatrix<T, I, J> &rhs,
-                         qmatrix<T, I, J> &out)
-          : op(op), lhs(lhs), rhs(rhs), out(out) {}
-
-      std::size_t size() const { return lhs.dim(); }
-
-      void do_work(const std::size_t row_index, const std::size_t) {
+#pragma omp parallel for num_threads(num_threads)
+      for (std::size_t row_index = 0; row_index < lhs.dim(); ++row_index) {
         auto lhs_row_begin = lhs.row_begin(row_index);
         auto lhs_row_end = lhs.row_end(row_index);
         auto rhs_row_begin = rhs.row_begin(row_index);
@@ -130,21 +114,6 @@ namespace quspin {
           }
         }
       }
-    };
-
-    template <typename Op, PrimativeTypes T, PrimativeTypes I, PrimativeTypes J>
-    void elementwise_binary_operation(Op &&op, const qmatrix<T, I, J> &lhs,
-                                      const qmatrix<T, I, J> &rhs, qmatrix<T, I, J> &out,
-                                      const std::size_t num_threads = 0) {
-      static_assert(std::is_invocable_v<Op, T, T>, "Incompatible Operator with input types");
-      static_assert(std::is_same_v<T, std::decay_t<std::invoke_result_t<Op, T, T>>>,
-                    "Incompatible output type");
-      assert(lhs.dim() == rhs.dim());
-      assert(lhs.dim() == out.dim());
-
-      populate_row_tasks<Op, T, I, J> tasks(op, lhs, rhs, out);
-      WorkQueue task_runner(tasks);
-      task_runner.run(Schedule::SequentialBlocks, num_threads);
     }
   }  // namespace details
 }  // namespace quspin
